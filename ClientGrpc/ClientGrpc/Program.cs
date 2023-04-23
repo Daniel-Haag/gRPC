6c7fe777576e479f6ed224d1145157762ad60ad3
Daniel Haag <danni.haag91@gmail.com>
2023-04-23 01:57:22 -0300

    primeiro commit

---
+++ ClientGrpc/ClientGrpc/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using ServicoGrpc;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientGrpc
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Daniel Haag" });
+            Console.WriteLine("Saudação: " + reply.Message);
+            Console.WriteLine("Pressione qualquer coisa para sair...");
+            Console.ReadKey();
+        }
+    }
+}
